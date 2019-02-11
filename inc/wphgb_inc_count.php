2392ed78dc064237da1b922f70341d25426286f3
m266 <herbrand@mailbox.org>
2019-02-11 01:50:36 +0100

    Delete wphgb_inc_count.php

--- inc/wphgb_inc_count.php
+++
@@ -1,7 +0,0 @@
-<?php
-// GB-ID ermitteln
-$options = get_option('wphgb_settings');
-$gb_id = $options['wphgb_text_field_0'];
-// GB-Einträge zählen
-$comments_count = wp_count_comments($gb_id);
-?>
